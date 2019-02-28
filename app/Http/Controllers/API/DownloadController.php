35f75e68edbddef98d25b9725ad29922335627ab
Hossein Jahangirian <jahangirianh@gmail.com>
2019-02-28 23:57:00 +0330

    add file size to download header

--- app/Http/Controllers/API/DownloadController.php
+++ app/Http/Controllers/API/DownloadController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth; 
 use App\User;
 use App\UserHasContent;
+use Illuminate\Support\Facades\Storage;
 
 class DownloadController extends Controller
 {
